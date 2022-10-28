584470685522181d5e622fd0bfee402cc61be6c0
pierre <pierregobin@free.Fr>
2022-10-28 07:39:47 +0200

    add vector type

--- bin/main.ml
+++ bin/main.ml
@@ -6,3 +6,9 @@ let () =
   let date = { year = 1970; month = 1; day = 1 } in
   printf "date = %s\n" (Hello_j.string_of_date date)
 
+let () = 
+        let v,w = ({x=8; y = -9; z=None } , {x=1; y=3; z= Some 9}) in
+        printf "x=%d, y=%d\n" v.x  v.y;
+        printf "v=%s\n" (Hello_j.string_of_vector_v1 v);
+        printf "w=%s\n" (Hello_j.string_of_vector_v1 w)
+
